a6930994cd8a75e7ad7a7e48a58ad7749746f7d7
Yarddog <Yarddog@DESKTOP-E4FOLBB>
2017-01-16 15:45:44 -0500

    githup push

---
+++ CallCompliance/Models/UnblockNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CallCompliance.Models {
+    public class UnblockNumbers {
+        public string PhoneNumber { get; set; }
+        public int ReasonOverrideId { get; set; }
+        public int StudentTypeId { get; set; }
+        public string Notes { get; set; }
+        public DateTime ModifiedBy { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}

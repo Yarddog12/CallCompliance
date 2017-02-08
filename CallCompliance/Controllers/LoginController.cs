3e9deba38a55b38d63cfb8a9658638d29c3af4c0
Yarddog12 <jqbeckwith@gmail.com>
2017-02-08 17:49:38 -0500

    fixing login

--- CallCompliance/Controllers/LoginController.cs
+++ CallCompliance/Controllers/LoginController.cs
@@ -11,7 +11,9 @@ namespace CallCompliance.Controllers
 		public ActionResult Index() {
 
 			var model = new LoginViewModel();
+			_logger.Info ("LoginController/Index()");
 			return View(model);
+
         }
 
 		[HttpPost]
@@ -21,29 +23,53 @@ namespace CallCompliance.Controllers
 
 			var model = new LoginViewModel();
 
-			if (!string.IsNullOrEmpty(vm.UserName) || !string.IsNullOrEmpty(vm.Password)) {
+			string userPrincipalName = vm.UserName + "@" + "ULTIMATEMEDICAL.LOCAL";
+			string password = vm.Password;
 
-				try {
-					using (var context = new PrincipalContext (ContextType.Domain)) {
-
-						var principal = UserPrincipal.FindByIdentity (context, User.Identity.Name);
-
-						if (principal != null) {
-							model.FullName = principal.DisplayName;					// ReqestorName
-							model.UserName = principal.SamAccountName.ToUpper ();	// RequestId
-							model.Department = "App Dev";							// ReqDepartment TODO: put all of this in a base class, and find department name.
-						} else {
-							return View(vm);
-						}
+			try {
+				using (var context = new PrincipalContext(ContextType.Domain, "ULTIMATEMEDICAL.LOCAL")) {
+					_logger.Info ("BEFORE context.ValidateCredentials(" + userPrincipalName + "," + password + ")");
+					if (context.ValidateCredentials(userPrincipalName, password)) {
+						_logger.Info ("BEFORE principal " );
+						var principal = UserPrincipal.FindByIdentity (context, userPrincipalName);
+						_logger.Info("AFTER " + principal?.GivenName);
+						return RedirectToAction("Welcome", "Home");
 					}
 				}
-				catch (Exception ex) {
-					return View(vm);
-				}
 			}
-			else {
-				return View(vm);
+			catch (Exception ex) {
+				_logger.Error(ex, "login failed.");
+				return View (vm);
 			}
+
+
+			//if (!string.IsNullOrEmpty(vm.UserName) || !string.IsNullOrEmpty(vm.Password)) {
+			//	_logger.Info ("LoginController/Index(LoginViewModel vm) past if null check");
+			//	try {
+			//		using (var context = new PrincipalContext (ContextType.Domain)) {
+
+			//			var principal = UserPrincipal.FindByIdentity (context, User.Identity.Name);
+			//			_logger.Info ("LoginController/Index(LoginViewModel vm) principal = " + principal);
+			//			if (principal != null) {
+			//				model.FullName = principal.DisplayName;					// ReqestorName
+			//				model.UserName = principal.SamAccountName.ToUpper ();	// RequestId
+			//				model.Department = "App Dev";                           // ReqDepartment TODO: put all of this in a base class, and find department name.
+			//				_logger.Info ("LoginController/Index(LoginViewModel vm) principal.DisplayName = " + principal.DisplayName);
+			//			} else {
+			//				_logger.Info("LoginController/Index(LoginViewModel vm principle must be null");
+			//				return View(vm);
+			//			}
+			//		}
+			//	}
+			//	catch (Exception ex) {
+			//		_logger.Error(ex,"LoginController/Index(LoginViewModel vm principle must be null");
+			//		return View(vm);
+			//	}
+			//}
+			//else {
+			//	_logger.Info ("LoginController/Index(LoginViewModel vm values must be null");
+			//	return View(vm);
+			//}
 
 			// This is good.
 			return RedirectToAction("Welcome", "Home");
